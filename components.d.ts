5ed0bd2c0e2830844c8d6545e8463853323aa357
ccnuzw <ccnuzw@gmail.com>
2025-11-11 16:46:34 +0800

    修复配置文件预览订阅时在全部策略下的重复请求问题

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module 'vue' {
     ConversionSettings: typeof import('./src/components/settings/ConversionSettings.vue')['default']
     GeneralSettings: typeof import('./src/components/settings/GeneralSettings.vue')['default']
     Layout: typeof import('./src/components/Layout.vue')['default']
-    NInput: typeof import('naive-ui')['NInput']
     NSpin: typeof import('naive-ui')['NSpin']
     NText: typeof import('naive-ui')['NText']
     ProfileForm: typeof import('./src/components/settings/ProfileForm.vue')['default']
